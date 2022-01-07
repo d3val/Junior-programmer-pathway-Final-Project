dd1c6b524b06d9b462f00ac4cc7759a16ee3f5c9
d3val <devalt5008@gmail.com>
2022-01-06 18:18:01 -0600

    Spawner script
    -Spawner can instatiate a GameObject using a raycast from the main camera in the world

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    static Vector3 spawnPosition;
+
+   public static void SpawnWithRaycast(GameObject prefab)
+    {
+        Ray ray;
+        RaycastHit hit;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            spawnPosition = hit.point;
+        }
+        Instantiate(prefab, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/UIMainManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainManager : MonoBehaviour
+{
+    public GameObject unitPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawner.SpawnWithRaycast(unitPrefab);
+        }
+    }
+}
